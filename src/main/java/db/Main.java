f57bd9a9a4971cc4ec59849429705945d40db763
Asus <olehh3103@gmail.com>
2022-12-23 16:01:41 +0200

    commit 2

---
+++ src/main/java/db/Main.java
@@ -0,0 +1,11 @@
+package db;
+
+public class Main {
+    public static void main(String[] args) {
+        БазаДаних db = new БазаДаних();
+        Авторизація авторизація = new Авторизація();
+        if (авторизація.авторизуватися(db)) {
+            ReportBuilder br = new ReportBuilder(db);
+        }
+    }
+}
